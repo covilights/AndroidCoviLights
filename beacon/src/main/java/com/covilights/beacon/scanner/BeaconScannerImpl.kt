23c98c6582178ef09aa6badf45370fa29ad400cd
Mohsen Mirhoseini <msn.mirhoseini@gmail.com>
2020-04-12 16:36:28 +0200

    Setup KtLint and reformat

--- beacon/src/main/java/com/covilights/beacon/scanner/BeaconScannerImpl.kt
+++ beacon/src/main/java/com/covilights/beacon/scanner/BeaconScannerImpl.kt
@@ -111,14 +111,22 @@ internal class BeaconScannerImpl(private val config: BeaconConfigProvider, priva
 
     private fun getErrorCodeMessage(errorCode: Int): String {
         return when (errorCode) {
-            NO_ERROR -> "The requested scanning was successful."
-            ScanCallback.SCAN_FAILED_ALREADY_STARTED -> "Fails to start scan as BLE scan with the same settings is already started by the app."
-            ScanCallback.SCAN_FAILED_APPLICATION_REGISTRATION_FAILED -> "Fails to start scan as app cannot be registered."
-            ScanCallback.SCAN_FAILED_INTERNAL_ERROR -> "Fails to start scan due an internal error"
-            ScanCallback.SCAN_FAILED_FEATURE_UNSUPPORTED -> "Fails to start power optimized scan as this feature is not supported."
-            SCAN_FAILED_OUT_OF_HARDWARE_RESOURCES -> "Fails to start scan as it is out of hardware resources."
-            SCAN_FAILED_SCANNING_TOO_FREQUENTLY -> "Fails to start scan as application tries to scan too frequently."
-            else -> "Unknown error!"
+            NO_ERROR ->
+                "The requested scanning was successful."
+            ScanCallback.SCAN_FAILED_ALREADY_STARTED ->
+                "Fails to start scan as BLE scan with the same settings is already started by the app."
+            ScanCallback.SCAN_FAILED_APPLICATION_REGISTRATION_FAILED ->
+                "Fails to start scan as app cannot be registered."
+            ScanCallback.SCAN_FAILED_INTERNAL_ERROR ->
+                "Fails to start scan due an internal error"
+            ScanCallback.SCAN_FAILED_FEATURE_UNSUPPORTED ->
+                "Fails to start power optimized scan as this feature is not supported."
+            SCAN_FAILED_OUT_OF_HARDWARE_RESOURCES ->
+                "Fails to start scan as it is out of hardware resources."
+            SCAN_FAILED_SCANNING_TOO_FREQUENTLY ->
+                "Fails to start scan as application tries to scan too frequently."
+            else ->
+                "Unknown error!"
         }
     }
 
