4f7dd9f159cad25dc659f59d646834f4330f7d4f
Mohsen Mirhoseini <msn.mirhoseini@gmail.com>
2020-04-20 15:52:13 +0200

    Improve koin

--- app/src/main/java/com/covilights/view/splash/SplashFragment.kt
+++ app/src/main/java/com/covilights/view/splash/SplashFragment.kt
@@ -9,12 +9,19 @@ import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import com.covilights.databinding.SplashFragmentBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.unloadKoinModules
 
 class SplashFragment : Fragment() {
 
     lateinit var binding: SplashFragmentBinding
 
     private val viewModel: SplashViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        loadKoinModules(splashModule)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,5 +37,10 @@ class SplashFragment : Fragment() {
         })
 
         return view
+    }
+
+    override fun onDestroy() {
+        unloadKoinModules(splashModule)
+        super.onDestroy()
     }
 }
