45905540c503f4ac073793e831d479e03f2900f6
Ostap <ostap.opalynksyi@germanautolabs.com>
2020-04-26 12:55:29 +0300

    Bind text and colors in viewModel https://github.com/covilights/AndroidCoviLights/issues/7

--- app/src/main/java/com/covilights/view/main/MainModule.kt
+++ app/src/main/java/com/covilights/view/main/MainModule.kt
@@ -16,9 +16,10 @@
 
 package com.covilights.view.main
 
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val mainModule = module {
-    viewModel { MainViewModel() }
+    viewModel { MainViewModel(context = androidContext(), userManager = get()) }
 }
--- app/src/main/java/com/covilights/view/main/MainViewModel.kt
+++ app/src/main/java/com/covilights/view/main/MainViewModel.kt
@@ -16,22 +16,87 @@
 
 package com.covilights.view.main
 
+import android.content.Context
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavDirections
+import com.covilights.R
+import com.covilights.user.UserManager
+import com.covilights.user.UserStatus
 
-class MainViewModel : ViewModel() {
+class MainViewModel(val context: Context, val userManager: UserManager) : ViewModel() {
 
     private val _navigate = MutableLiveData<NavDirections>()
     val navigate: LiveData<NavDirections>
         get() = _navigate
+    val status: LiveData<UserStatus>
+        get() = _status
+    private val _status: MutableLiveData<UserStatus> = MutableLiveData(userManager.userStatus)
 
-    fun onDebugClick() {
-        _navigate.value = MainFragmentDirections.actionMainFragmentToDebugFragment()
+    val statusIcon: LiveData<Drawable> = Transformations.map(_status) { userStatus ->
+        val resourceId = when (userStatus) {
+            UserStatus.NEGATIVE -> R.drawable.ic_confirm
+            UserStatus.EXPOSED -> R.drawable.ic_attention
+            UserStatus.POSITIVE -> R.drawable.ic_alert
+        }
+        ContextCompat.getDrawable(context, resourceId)
     }
 
-    fun onDesignClick() {
-        _navigate.value = MainFragmentDirections.actionMainFragmentToDesignFragment()
+    val statusIconColor: LiveData<Int> = Transformations.map(_status) { userStatus ->
+        val resourceId = when (userStatus) {
+            UserStatus.POSITIVE,
+            UserStatus.NEGATIVE -> R.color.status_ok_icon
+            UserStatus.EXPOSED -> R.color.primary
+        }
+        ContextCompat.getColor(context, resourceId)
+    }
+
+    val statusTextColor: LiveData<Int> = Transformations.map(_status) { userStatus ->
+        val resourceId = when (userStatus) {
+            UserStatus.POSITIVE,
+            UserStatus.NEGATIVE -> R.color.status_ok_icon
+            UserStatus.EXPOSED -> R.color.primary
+        }
+        ContextCompat.getColor(context, resourceId)
+    }
+
+    val statusTitle: LiveData<String> = Transformations.map(_status) { userStatus ->
+        val resourceId = when (userStatus) {
+            UserStatus.NEGATIVE -> R.string.main_status_ok_title
+            UserStatus.EXPOSED -> R.string.main_status_exposed_title
+            UserStatus.POSITIVE -> R.string.main_status_positive_title
+        }
+        context.getString(resourceId)
+    }
+
+    val statusSubtitle: LiveData<String> = Transformations.map(_status) { userStatus ->
+        val resourceId = when (userStatus) {
+            UserStatus.NEGATIVE -> R.string.main_status_ok_subtitle
+            UserStatus.EXPOSED -> R.string.main_status_exposed_subtitle
+            UserStatus.POSITIVE -> R.string.main_status_positive_subtitle
+        }
+        context.getString(resourceId)
+    }
+
+    val statusBackgroundColor: LiveData<Int> = Transformations.map(_status) { userStatus ->
+        val resourceId = when (userStatus) {
+            UserStatus.NEGATIVE -> R.color.status_ok_bg
+            UserStatus.EXPOSED -> R.color.status_exposed_bg
+            UserStatus.POSITIVE -> R.color.status_positive_bg
+        }
+        ContextCompat.getColor(context, resourceId)
+    }
+
+    val statusMoreInfoBgColor: LiveData<Int> = Transformations.map(_status) { userStatus ->
+        val resourceId = when (userStatus) {
+            UserStatus.NEGATIVE -> R.color.status_ok_more_info_bg
+            UserStatus.EXPOSED -> R.color.status_exposed_more_info_bg
+            UserStatus.POSITIVE -> R.color.status_positive_more_info_bg
+        }
+        ContextCompat.getColor(context, resourceId)
     }
 }
