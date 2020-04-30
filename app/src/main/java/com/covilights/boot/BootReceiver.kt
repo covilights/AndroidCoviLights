c0e5a1e65db3375902f2dae9185ddbfead0f8af6
Mohsen Mirhoseini <msn.mirhoseini@gmail.com>
2020-04-30 15:59:49 +0200

    Fix detekt CollapsibleIfStatements

--- app/src/main/java/com/covilights/boot/BootReceiver.kt
+++ app/src/main/java/com/covilights/boot/BootReceiver.kt
@@ -25,8 +25,8 @@ import com.covilights.service.BeaconServiceActions
 class BootReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent) {
-        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-            if (context != null) ContextCompat.startForegroundService(context, BeaconServiceActions.DeviceBoot.toIntent(context))
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED && context != null) {
+            ContextCompat.startForegroundService(context, BeaconServiceActions.DeviceBoot.toIntent(context))
         }
     }
 }
