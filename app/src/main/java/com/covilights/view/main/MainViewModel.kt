713132e15d4cf0d3769b143f7b69b1e653edc337
Mohsen Mirhoseini <msn.mirhoseini@gmail.com>
2020-04-30 17:55:57 +0200

    Add debug and design buttons to MainFragment
    They show up in case of debug build or long press on app icon in the
    toolbar as Easter egg.

--- app/src/main/java/com/covilights/view/main/MainViewModel.kt
+++ app/src/main/java/com/covilights/view/main/MainViewModel.kt
@@ -18,14 +18,30 @@ package com.covilights.view.main
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavDirections
+import com.covilights.BuildConfig
 
 internal class MainViewModel : ViewModel() {
 
     private val _navigate = MutableLiveData<NavDirections>()
     val navigate: LiveData<NavDirections>
         get() = _navigate
+
+    private val debugMoodeEnabled = MutableLiveData(BuildConfig.DEBUG)
+    val isDebugMode: LiveData<Boolean> = Transformations.map(debugMoodeEnabled) { isDebugEnabled ->
+        isDebugEnabled || BuildConfig.DEBUG
+    }
+
+    fun onToolbarLongClick(): Boolean {
+        return if (debugMoodeEnabled.value != true) {
+            debugMoodeEnabled.value = true
+            true
+        } else {
+            false
+        }
+    }
 
     fun onDebugClick() {
         _navigate.value = MainFragmentDirections.actionMainFragmentToDebugFragment()
